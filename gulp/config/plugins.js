ba9b6198d4a6f9ff8a4d25e06dd1ada30eee99a4
prismagod <sofuntobenoname@protonmail.com>
2022-03-03 21:06:27 +0300

    scss gulp

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,5 +1,11 @@
-import replace from "gulp-replace";
+import replace from "gulp-replace"; // search and replace
+import plumber from "gulp-plumber"; // error processing
+import notify from "gulp-notify"; // notifier
+import browsersync from "browser-sync"; // live server
 
 export const plugins = {
-	replace: replace
+	replace: replace,
+	plumber: plumber,
+	notify: notify,
+	browsersync: browsersync
 }
--- gulpfile.js
+++ gulpfile.js
@@ -18,18 +18,20 @@ global.app = {
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
-import replace from "gulp-replace";
+import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
 
 // watcher
 function watcher() {
 	gulp.watch(path.watch.files, copy);
 	gulp.watch(path.watch.html, html);
+	gulp.watch(path.watch.scss, scss);
 }
 
-const mainTasks = gulp.parallel(copy, html);
+const mainTasks = gulp.parallel(copy, html, scss);
 
 // scenario builder
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 // default scenario
 gulp.task('default', dev);
